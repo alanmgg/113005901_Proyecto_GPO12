42afc5418d692b2260a1279a5fb9c7172ff95287
alanmgg <afmg.unam@gmail.com>
2022-04-04 21:02:18 -0500

    Add models

--- ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
@@ -141,7 +141,7 @@ int main( )
     glEnableVertexAttribArray(2);
 
     // Load textures
-    Model sillon((char*)"Models/Sillon/sillon.obj");
+    Model mesa((char*)"Models/Table/table.obj");
 
 
     GLuint texture;
@@ -187,7 +187,7 @@ int main( )
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
 
 
-        sillon.Draw(shader);
+        mesa.Draw(shader);
 
         
         glActiveTexture(GL_TEXTURE0);
